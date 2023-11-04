7faf222145a301d79b5b611ed660d76313f0f326
FELIPE\fe_ro <101447@aluno.uricer.edu.br>
2023-11-03 23:12:43 -0300

    Adicionado o express e server.js ao projeto

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const {Pool} = require('pg');
+
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+const pool = new Pool({
+    user: 'postgres',
+    host: 'localhost',
+    database: 'postgres',
+    password: 'admin',
+    port: 5432,
+  });
+
